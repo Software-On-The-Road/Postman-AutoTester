d0f45a39813c89b5945096171c0a561085a091c2
Santiago Quinteros <contacto.quinteros@gmail.com>
2018-08-21 19:00:53 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
+const data = {
+  a: 1,
+  b: 2,
+  c: {
+    d: 3,
+    e: {
+      f: 4,
+      g: 5
+    },
+    h: 6
+  },
+  i: {
+    j: {
+      k: 7
+    }
+  }
+};
 
+deepMap(data, null, postmanStrategy)
