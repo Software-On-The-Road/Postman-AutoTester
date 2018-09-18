02fd9eb1bf50721c788a82d22e4e7b481505efec
Santiago Quinteros <contacto.quinteros@gmail.com>
2018-09-17 21:13:03 -0300

    Update postman.js

--- src/strategies/postman.js
+++ src/strategies/postman.js
@@ -1,3 +1,3 @@
 function postmanStrategy(parent, prop, type) {
-  return 'pm.test("It should have ' + prop + ' property", function() {\n \t var jsonData = pm.response.json(); \n\t return pm.expect(jsonData.' + (parent ? parent + '.' : '') + prop + ').to.exist.and.to.be.a(\'' + (type || 'string') + '\')); \n}';
+  return 'pm.test("It should have ' + prop + ' property", function() {\n \t var jsonData = pm.response.json(); \n\t return pm.expect(jsonData.' + (parent ? parent + '.' : '') + prop + ').to.exist.and.to.be.a(\'' + (type || 'string') + '\'); \n}';
 }
