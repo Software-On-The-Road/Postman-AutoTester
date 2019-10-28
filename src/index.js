06f7edd0a1caefd4dba1060a4584f0ebf2ca6062
unknown <flepa@hotmail.com>
2019-10-28 10:22:25 -0300

    Api Styles - Postman-AutoTestApi

--- src/index.js
+++ src/index.js
@@ -32,3 +32,26 @@ function runConverter() {
 
   output.innerHTML = result;
 }
+
+
+
+// buttom copy
+var copyTextareaBtn = document.querySelector('.button__copy');
+
+copyTextareaBtn.addEventListener('click', function(event) {
+  var copyTextarea = document.querySelector('.form-control');
+  copyTextarea.select();
+
+  try {
+    var successful = document.execCommand('copy');
+    var msg = successful ? 'successful' : 'unsuccessful';
+    console.log('Copying text command was ' + msg);
+  } catch (err) {
+    console.log('Oops, unable to copy');
+  }
+});
+
+// button undo
+function undoTextArea() {
+  document.execCommand("undo", false, null);
+}
